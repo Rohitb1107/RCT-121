a3432b39aa9653c0412b21515364183ccbc77637
Rohit <stylishrb711@gmail.com>
2022-05-19 23:37:33 +0530

    menu card created

--- my-restaurant/src/App.jsx
+++ my-restaurant/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import MenuCard from "./Components/MenuCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      <MenuCard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-restaurant/src/Components/MenuCard.jsx
+++ my-restaurant/src/Components/MenuCard.jsx
@@ -1,9 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
 const MenuCard = () => {
   return (
     <>
       <h1>MenuCard</h1>
+      <div className="card">
+        <div className="left-part">
+          <div className="left-img">
+            <img
+              src="https://avatarfiles.alphacoders.com/893/thumb-89303.gif"
+              alt=""
+            />
+          </div>
+          <div className="right-in-left-part">
+            <div className="dish-name">URU Brewpark</div>
+            <div className="dish-type">Pizza, Asian, Desserts</div>
+            <div className="rate-time">
+              <span>Min 50Rs. </span> <span>Up to 30 min</span>
+            </div>
+            <div className="pay-method">Accepts online payments only</div>
+          </div>
+        </div>
+        <div className="right-part">
+          <div className="ratings">4.3</div>
+          <div className="vote">766 votes</div>
+          <div className="reviews">512 reviews</div>
+        </div>
+        <div className="order-part">
+          <button>Add to cart</button>
+        </div>
+        <div className="order-part">
+          <button>Order Now</button>
+        </div>
+      </div>
     </>
   );
 };
