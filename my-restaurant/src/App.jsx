3d5d4e97e6e0f8299e27067e96f09d9380f56f44
Rohit <stylishrb711@gmail.com>
2022-05-21 15:31:29 +0530

    data fetched

--- my-restaurant/src/App.jsx
+++ my-restaurant/src/App.jsx
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import MenuCard from "./Components/MenuCard";
+import axios from "axios";
 
 function App() {
-useEffect(() => {
-  
-})
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/food")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="App">
       <MenuCard />
