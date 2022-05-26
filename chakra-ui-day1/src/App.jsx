b123d480f906d4ae497d93f9d4a4baa86f19efc0
Rohit <stylishrb711@gmail.com>
2022-05-26 21:56:53 +0530

    grid done

--- chakra-ui-day1/src/App.jsx
+++ chakra-ui-day1/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import FlexFunc from "./Components/FlexFunc";
+import GridFunc from "./Components/GridFunc";
 
 function App() {
   return (
     <div className="App">
-      <FlexFunc />
+      {/* <FlexFunc /> */}
+      <GridFunc />
     </div>
   );
 }
---
+++ chakra-ui-day1/src/Components/GridFunc.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Grid, GridItem, Center, Text } from "@chakra-ui/react";
+
+const FlexFunc = () => {
+  return (
+    <>
+      <div>
+        <Grid templateColumns="1fr 2fr 1fr">
+          <GridItem w="100%" h="120" bg="blue.500">
+            <Center>
+              <Text>Nav</Text>
+            </Center>
+          </GridItem>
+          <GridItem w="100%" h="120" bg="yellow.500">
+            <Center>
+              <Text>Content</Text>
+            </Center>
+          </GridItem>
+          <GridItem w="100%" h="120" bg="red.500">
+            <Center>
+              <Text>Content</Text>
+            </Center>
+          </GridItem>
+        </Grid>
+      </div>
+    </>
+  );
+};
+
+export default FlexFunc;
