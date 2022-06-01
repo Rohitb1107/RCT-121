2c1808c9717aa7b7b6ac6fc6599bf949daa516bb
Rohit <stylishrb711@gmail.com>
2022-06-01 23:31:32 +0530

    components created

--- learn-context/src/context/AppContextProvider.jsx
+++ learn-context/src/context/AppContextProvider.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const AppContext = React.createContext();
 
-const AppContextProvider = () => {
+const AppContextProvider = ({ children }) => {
+  const [state, setState] = useState("light");
+
+  const changeTheme = (theme) => {
+    state === "light" ? setState("dark") : setState("light");
+  };
+
   return (
-    <AppContext.Provider value={[{}, () => {}]}>
+    <AppContext.Provider value={[state, changeTheme]}>
       <div>AppContextProvider</div>
     </AppContext.Provider>
   );
