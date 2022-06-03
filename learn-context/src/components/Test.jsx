1900b9280764ae2050c56fc981c8243d51b05c79
Rohit <stylishrb711@gmail.com>
2022-06-03 22:43:58 +0530

    theme part done

--- learn-context/src/components/Test.jsx
+++ learn-context/src/components/Test.jsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
-import { AppContext } from "../context/AppContextProvider";
-const test = () => {
-  const [state, name, toggleTheme] = useContext(AppContext);
-  console.log(name);
-  return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  );
-};
+// import React, { useContext } from "react";
+// import { AppContext } from "../context/AppContextProvider";
+// const test = () => {
+//   const [state, name, toggleTheme] = useContext(AppContext);
+//   console.log(name);
+//   return (
+//     <div>
+//       <h1>Test</h1>
+//     </div>
+//   );
+// };
 
-export default test;
+// export default test;
--- learn-context/src/context/AppContextProvider.jsx
+++ learn-context/src/context/AppContextProvider.jsx
@@ -4,14 +4,14 @@ export const AppContext = React.createContext();
 
 const AppContextProvider = ({ children }) => {
   const [state, setState] = useState("light");
-  const [name, setName] = useState("Rohit");
+  // const [name, setName] = useState("Rohit");
 
   const toggleTheme = () => {
     state === "light" ? setState("dark") : setState("light");
   };
 
   return (
-    <AppContext.Provider value={[state, name, toggleTheme]}>
+    <AppContext.Provider value={[state, toggleTheme]}>
       {children}
     </AppContext.Provider>
   );
