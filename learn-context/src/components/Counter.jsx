5a16aa91add3a1802d66af954d12c26d691f4450
Rohit <stylishrb711@gmail.com>
2022-06-04 07:29:44 +0530

    useReducer works

--- learn-context/src/components/Counter.jsx
+++ learn-context/src/components/Counter.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const initialState = {
+  counter: 0,
+  clicks: 0,
+};
+
+function reducer(state, action) {
+  if (action.type === "INCREMENT") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "DECREMENT") {
+    return {
+      ...state,
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+}
 
 const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div>
-      <h1>Learn UseReducer</h1>
-    </div>
+    <>
+      <div>
+        <h1>{state.counter}</h1>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>Add</button>
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>Remove</button>
+      </div>
+    </>
   );
 };
 
