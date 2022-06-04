e429e150132e4bba2f7eb02901440d3bfe3b0384
Rohit <stylishrb711@gmail.com>
2022-06-04 14:01:46 +0530

    context created

---
+++ react-router-1/src/Context/AuthContext.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const AuthContext = React.createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
+  return (
+    <div>
+      <AuthContextProvider value={[isAuth, setIsAuth]}>
+        {children}
+      </AuthContextProvider>
+    </div>
+  );
+};
+
+export default AuthContextProvider;
