1c4e6c120e53d3ddf0fedffb82c750139fa494fe
Rohit <stylishrb711@gmail.com>
2022-06-05 13:34:05 +0530

    login update

--- react-router-1/src/components/Login.jsx
+++ react-router-1/src/components/Login.jsx
@@ -1,9 +1,27 @@
+import axios from "axios";
 import React, { useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
 
 const Login = () => {
   const [isAuth, setIsAuth] = useContext(AuthContext);
+
+  const loginUser = async () => {
+    axios({
+      url: "https://reqres.in/api/login",
+      method: "POST",
+      data: {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka",
+      },
+    })
+      .then((res) => {
+        alert("Success");
+      })
+      .catch((err) => {
+        alert("Login failed");
+      });
+  };
 
   if (isAuth) {
     return <Navigate to="/" />;
