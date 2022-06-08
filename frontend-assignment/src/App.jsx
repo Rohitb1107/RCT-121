f59453f630b70cd0a6a43661834c1feb25c3a8cc
Rohit <stylishrb711@gmail.com>
2022-06-08 22:47:56 +0530

    comment added

--- frontend-assignment/src/App.jsx
+++ frontend-assignment/src/App.jsx
@@ -11,3 +11,8 @@ function App() {
 }
 
 export default App;
+
+
+// This is an ecommerce site created via React Js for frontend.
+// And I used Json-server for backend so pls first run (npm run json-server).
+// to see the project. otherwise it shows nothing. Thank You.
